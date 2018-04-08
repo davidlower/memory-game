1b1a2f5401d0a4b22231c9ffbcc4c6d776cf8fe1
David Lower <davidlower4@gmail.com>
2018-04-08 20:06:55 +0100

    added a success and fail animation to flipped cards

--- main.js
+++ main.js
@@ -1,91 +1,154 @@
-// <i class="fas fa-car"></i>
-// <i class="fas fa-bomb"></i>
-// <i class="fas fa-bell"></i>
-// <i class="fas fa-bowling-ball"></i>
-// <i class="fas fa-camera"></i>
-// <i class="fas fa-cloud"></i>
-// <i class="fas fa-key"></i>
-// <i class="fas fa-shopping-cart"></i>
-
-var iconsArray = [
-   'fa-shopping-cart card-1',
-   'fa-shopping-cart card-1',
-   'fa-car card-2',
-   'fa-car card-2',
-   'fa-bomb card-3',
-   'fa-bomb card-3',
-   'fa-bell card-4',
-   'fa-bell card-4',
-   'fa-bowling-ball card-5',
-   'fa-bowling-ball card-5',
-   'fa-camera card-6',
-   'fa-camera card-6',
-   'fa-cloud card-7',
-   'fa-cloud card-7',
-   'fa-key card-8',
-   'fa-key card-8'
-];
-var doesItMatchArray = [];
-var itemArray = [];
-var counter = 0;
-var starsArray = [];
-
-// SHUFFLE FUCNTION - to randomise the various icons used in the game.
-Array.prototype.shuffle = function() {
-   for (i = 1; i < this.length; i++) {
-      random = Math.round(Math.random() * i);
-      temp = this[random];
-      this[random] = this[i];
-      this[i] = temp;
-   }
-   return this;
-};
-
-// SETUP OF THE MEMORY BOARD - assigning all the icons to random cards
-function memoryGameBoard() {
-   var htmlCode = "";
-   var gameBoard = document.getElementById("memory-game-board");
-   iconsArray.shuffle();
-   for (i = 0; i < iconsArray.length; i++) {
-      htmlCode += '<div class="card" id="card' + i + '"><div class="back"><i class="fas ' + iconsArray[i] + '"></i></div></div>';
-   }
-   gameBoard.innerHTML = htmlCode;
-}
-memoryGameBoard();
-
-
 document.addEventListener("DOMContentLoaded", function(event) {
 
-   // REFRESHING OR RELOADING of the game board
+   var iconsArray = [
+      [1, 'fa-shopping-cart'],
+      [1, 'fa-shopping-cart'],
+      [2, 'fa-car'],
+      [2, 'fa-car'],
+      [3, 'fa-bomb'],
+      [3, 'fa-bomb'],
+      [4, 'fa-bell'],
+      [4, 'fa-bell'],
+      [5, 'fa-bowling-ball'],
+      [5, 'fa-bowling-ball'],
+      [6, 'fa-cloud'],
+      [6, 'fa-cloud'],
+      [7, 'fa-camera'],
+      [7, 'fa-camera'],
+      [8, 'fa-key'],
+      [8, 'fa-key']
+   ];
+
+
+   var match = [];
+   var item = [];
+   var counter = 0;
+   var moves = 0;
+   var stars = [];
+
+   var gameBoard = document.getElementById("memory-game-board");
    var refresh = document.getElementById("refresh");
+
+
+   // SHUFFLE FUCNTION - to randomise the various icons used in the game.
+   Array.prototype.shuffle = function() {
+      for (i = 1; i < this.length; i++) {
+         random = Math.round(Math.random() * i);
+         temp = this[random];
+         this[random] = this[i];
+         this[i] = temp;
+      }
+      return this;
+   };
+
+   // SETUP OF THE MEMORY BOARD - assigning all the icons to random cards
+   function memoryGameBoard() {
+      var htmlCode = "";
+      iconsArray.shuffle();
+      for (i = 0; i < iconsArray.length; i++) {
+         htmlCode += '<div class="card card' + iconsArray[i][0] + '"><i class="fas ' + iconsArray[i][1] + '"></i></div>';
+      }
+      gameBoard.innerHTML = htmlCode;
+   }
+   memoryGameBoard();
+
+   // FLIP CARD AND ASSIGNING A CLICK EVENT LISTENER
+   function flipCard() {
+      gameBoard.addEventListener("click", function(e) {
+         if (e.target.classList.contains("card")) {
+            e.target.classList.add("selected");
+            // e.target.style.transform = "rotateY(180deg)";
+            matchCards(e.target);
+            timer();
+         }
+      });
+   }
+   flipCard();
+
+
+
+   // CHECKING FOR MATCHED FLIPPED CARDS
+   function matchCards(element) {
+      var cardId = element.classList[1];
+      var selected = document.querySelectorAll(".selected");
+      if (item.length == 0) {
+         item.push(1);
+         match.push(cardId);
+         console.log(item);
+         console.log(match);
+      } else if (item.length == 1) {
+         item.push(1);
+         match.push(cardId);
+         moves++;
+         console.log(moves);
+         if (match[0] == match[1]) {
+            setTimeout(function() {
+               success();
+            }, 500);
+            counter += 2;
+            console.log(counter);
+            match = [];
+            item = [];
+            if (counter == iconsArray.length) {
+               popupModal();
+            }
+         } else {
+            console.log('it does not match');
+            setTimeout(function() {
+               fail();
+            }, 500);
+         }
+      }
+   }
+
+
+   function success() {
+      var selected = document.querySelectorAll(".selected");
+      for (i = 0; i < 2; i++) {
+         selected[i].classList.add("success");
+      }
+      setTimeout(function() {
+         selected[0].classList.remove("selected");
+         selected[1].classList.remove("selected");
+      }, 1000);
+   }
+
+   function fail() {
+      console.log("flipping you back");
+      var selected = document.querySelectorAll(".selected");
+      for (i = 0; i < 2; i++) {
+         selected[i].classList.add("fail");
+      }
+      setTimeout(function() {
+         selected[0].classList.remove("fail", "selected");
+         selected[1].classList.remove("fail", "selected");
+      }, 1000);
+   }
+
+   function popupModal() {
+      console.log("game has finished");
+   }
+
+
+   // TIMER function
+   function timer() {
+      var time = 0;
+      setTimeout(function() {
+         time++;
+         var mins = Math.floor(time / 10 / 60);
+         var seconds = Math.floor(time / 10);
+         if (mins < 10) {
+            mins = "0" + mins;
+         }
+         if (seconds < 10) {
+            seconds = "0" + seconds;
+         }
+         document.getElementById("timer").innerHTML = mins + ":" + seconds;
+         timer();
+      }, 500);
+   }
+
+   // REFRESHING OF THE GAME BOARD
    refresh.addEventListener("click", memoryGameBoard);
 
-   // FLIP CARD AND CLICK EVENT LISTENER
-   var deck = document.querySelector("#memory-game-board");
-   deck.addEventListener("click", function(event) {
-      if (event.target.classList.contains("card")) {
-         event.target.style.transform = "rotateY(-180deg)";
-         flipCard();
-      }
-   });
-
-   // COMPARING CARDS FUNCTION
-   function flipCard() {
-      var cardId = document.getElementsByTagName(i);
-      cardIdSelection = cardId.classList;
-      console.log(cardIdSelection);
-
-      // if (itemArray.length == 0) {
-      //    itemArray.push("a");
-      // } else if (itemArray.length == 1) {
-      //    itemArray.push("b");
-      // } else {
-      //    console.log("match");
-      // }
-      // console.log(itemArray);
-      console.log("clicked");
-
-
-
-   }
 });
