62c369fbb4bd783fef5b9b3e948fc82165d60839
David Lower <davidlower4@gmail.com>
2018-04-09 15:16:11 +0100

    links are working, cards and flipped with correct animation. Refresh button works. Modal popup window works and also closes

--- main.js
+++ main.js
@@ -28,9 +28,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
    var gameBoard = document.getElementById("memory-game-board");
    var refresh = document.getElementById("refresh");
+   var bodyId = document.getElementById("body-wrapper");
+   var modalId = document.getElementById("modal-wrapper");
+   var scoreId = document.getElementById("score");
+   var movesId = document.getElementById("moves");
+   var btnId = document.getElementById("btn");
+   var closeId = document.getElementById("close");
+   var movesCounter = document.getElementById("movesCounter");
+   var timerCounter = document.getElementById("timer");
 
 
-   // SHUFFLE FUCNTION - to randomise the various icons used in the game.
+   // *****************************************************************
+   // SHUFFLE FUCNTION - to randomise the various icons used in the game
    Array.prototype.shuffle = function() {
       for (i = 1; i < this.length; i++) {
          random = Math.round(Math.random() * i);
@@ -40,7 +49,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
       return this;
    };
+   // *****************************************************************
 
+
+   // *****************************************************************
    // SETUP OF THE MEMORY BOARD - assigning all the icons to random cards
    function memoryGameBoard() {
       var htmlCode = "";
@@ -49,56 +61,64 @@ document.addEventListener("DOMContentLoaded", function(event) {
          htmlCode += '<div class="card card' + iconsArray[i][0] + '"><i class="fas ' + iconsArray[i][1] + '"></i></div>';
       }
       gameBoard.innerHTML = htmlCode;
+      match = [];
+      item = [];
+      moves = 0;
+      counter = 0;
+      stars = [];
+      movesCounter.innerHTML = "0 moves";
+      timerCounter.innerHTML = "00:00";
    }
    memoryGameBoard();
+   // *****************************************************************
 
+
+   // *****************************************************************
    // FLIP CARD AND ASSIGNING A CLICK EVENT
    function flipCard() {
       gameBoard.addEventListener("click", function(e) {
          if (e.target.classList.contains("card")) {
             e.target.classList.add("selected");
             matchCards(e.target);
-            timer();
+            timer(e.target);
+            gameMoves(e.target);
          }
       });
    }
    flipCard();
+   // *****************************************************************
 
+
+   // *****************************************************************
    // CHECKING FOR MATCHED FLIPPED CARDS
    function matchCards(element) {
       var cardId = element.classList[1];
-      var selected = document.querySelectorAll(".selected");
       if (item.length == 0) {
          item.push(1);
          match.push(cardId);
-         console.log(item);
-         console.log(match);
       } else if (item.length == 1) {
          item.push(1);
          match.push(cardId);
          moves++;
-         console.log(moves);
          if (match[0] == match[1]) {
-            console.log("there is a match");
             setTimeout(function() {
                success();
             }, 500);
             counter += 2;
-            console.log(counter);
             match = [];
             item = [];
             if (counter == iconsArray.length) {
                popupModal();
             }
          } else {
-            console.log('it does not match');
             setTimeout(function() {
                fail();
             }, 500);
+            match = [];
+            item = [];
          }
       }
    }
-
    // FUNCTION WHEN CARDS MATCH
    function success() {
       var selected = document.querySelectorAll(".selected");
@@ -109,10 +129,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
          selected[1].classList.remove("selected");
       }, 1000);
    }
-
    // FUNCTION WHEN CARDS FAIL TO MATCH
    function fail() {
-      console.log("flipping you back");
       var selected = document.querySelectorAll(".selected");
       selected[0].classList.add("fail");
       selected[1].classList.add("fail");
@@ -121,14 +139,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
          selected[1].classList.remove("fail", "selected");
       }, 1000);
    }
+   // *****************************************************************
 
+
+   // *****************************************************************
    // FUNCTION FOR WHEN GAME IS COMPLETE AND MODAL POPS UP WITH CONGRATULATIONS
    function popupModal() {
-      console.log("game has finished");
+      bodyId.style.display = "none";
+      modalId.style.display = "inline";
+      scoreId.innerHTML = "Your score was";
+      movesId.innerHTML = 'You did it in <span class=\"strong\">' + moves + '</span> moves.';
+   }
+   // CLOSING OF MODAL AND/OR STARTING A NEW GAME
+   function closeModal() {
+      bodyId.removeAttribute("style");
+      modalId.removeAttribute("style");
+      memoryGameBoard();
+   }
+   closeModal();
+   btnId.addEventListener("click", closeModal);
+   closeId.addEventListener("click", closeModal);
+   // REFRESHING OF THE GAME BOARD
+   refresh.addEventListener("click", memoryGameBoard);
+   // *****************************************************************
+
+
+   // *****************************************************************
+   // MOVES FUNCTION - COUNTING THE MOVES AND UPDATING THE stars
+   function gameMoves(element) {
+      movesCounter.innerHTML = moves + " moves";
+      if (moves <= 16) {
+
+      }
    }
 
+   // <i class="star-1 fas fa-star"></i>
+   // <i class="star-2 fas fa-star"></i>
+   // <i class="star-3 fas fa-star"></i>
+   // <i class="far fa-star"></i>
+   // *****************************************************************
 
-   // TIMER function
+
+   // *****************************************************************
+   // TIMER FUNCTION
    function timer() {
       var time = 0;
       setTimeout(function() {
@@ -141,12 +194,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
          if (seconds < 10) {
             seconds = "0" + seconds;
          }
-         document.getElementById("timer").innerHTML = mins + ":" + seconds;
-         timer();
+         timerCounter.innerHTML = mins + ":" + seconds;
       }, 500);
    }
-
-   // REFRESHING OF THE GAME BOARD
-   refresh.addEventListener("click", memoryGameBoard);
+   timer();
+   // *****************************************************************
 
 });
